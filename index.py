c3b90b9c18e2e958624f358b37ee9040cf4981c3
Fumiya <fumiya083077@gmail.com>
2022-02-01 19:24:46 +0900

    fix

---
+++ index.py
@@ -0,0 +1,72 @@
+#print("hello world!!")
+
+num = 1
+#print(num)
+
+#mozi = "おはよう"
+#print(mozi)
+
+#isok = True
+#isok = False
+#if isok:
+#    print("ok")
+#elif num<10:
+#    print("good")
+#else:
+#    print("ng")
+
+#for i in range(10):
+    #print(i)
+
+# for i in range(10):
+#     num = i+1
+#     tmp = []
+#     for j in range (num):
+#         if num % (j+1) ==0:
+#             tmp.append(num)
+#             print(tmp)
+#     if len(tmp) == 2:
+#         print(num)
+
+
+
+# for i in range(40):
+#     num = i+1
+#     if num%3==0 or num%4==0 or "3" in str(num):
+#         print("aho")
+#     else:
+#          print(num)
+
+def add(x,y):
+    return x*y+x/y
+
+
+res = add(3,5)
+print(res)
+
+rig = add(12,8)
+print(rig)
+
+# nn = "abcdefg"
+# print(nn.replace('a','b'))
+
+
+# print(type('string') is int)
+# print(type('string') is str)
+
+# よくでてくる注意
+# ci=pushしたときに自動でテストしてくれる
+# cd=commitしたときに自動でDEPURPI(サ−バ−側)してくれる
+
+# def is_str(v):
+#     return type(v) in (str,int)
+
+# print(is_str('string'))
+
+# print(is_str(100))
+
+# print(is_str([0, 1, 2]))
+
+# print(type(True))
+
+# print(type(False))
--- training.py
+++ training.py
@@ -1,3 +1,28 @@
-prinrt (hello world)
+print("hello world")
 
-#100*5+40*8
+# for i in range(40):
+#     num = i+1
+#     if num%4==0 and "2" in str(num):
+#         print("wao!")
+#         continue
+#     if num%3==0 and "3" in str(num):
+#         print("aho")
+#     else:
+#         print(num)
+
+
+for i in range(0,100,5):
+    num = i+1
+    if "1"in str(num):
+        print("zero")
+    if "10"in str(num):
+        print("tenten")
+    else:
+        print(num)
+
+
+# def add(x,y,z,r):
+#     return x*y+(x+z*r)
+
+# res = add(5,2,5,2)
+# print(res)
